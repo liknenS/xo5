b31acee42dc632c4387dd4d9ab851b8b95b2df8f
liknens <nik.nikitenok@gmail.com>
2018-02-26 17:52:56 +0300

    update webpack.config.prod.js

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -193,6 +193,7 @@ module.exports = {
                         // https://github.com/facebookincubator/create-react-app/issues/2677
                         ident: 'postcss',
                         plugins: () => [
+                          require('postcss-nested'),
                           require('postcss-flexbugs-fixes'),
                           autoprefixer({
                             browsers: [
